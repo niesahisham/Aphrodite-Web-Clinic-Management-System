81171355c95d369f6fed88f3dfbae7030f6859ef
aimibat <putriaimibatrisyia@gmail.com>
2026-06-12 09:45:35 +0800

    Add UserSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             PatientSeeder::class,
             DrugSeeder::class,
             PrescriptionSeeder::class,
